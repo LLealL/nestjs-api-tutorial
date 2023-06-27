c0a68cca76c15b68facd5a7b0c1346c6f1bf39e1
Lucas <30868253+LLealL@users.noreply.github.com>
2023-06-27 20:59:22 -0300

    Adição de logica de Edit User com testes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
 import { AuthDto } from '../src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App e2e', () =>{
   let app: INestApplication;
@@ -106,7 +107,20 @@ describe('App e2e', () =>{
       });
     });
 
-    describe('Edit user', () => {});
+    describe('Edit user', () => {
+      it('Should edit user', () =>{
+        const dto: EditUserDto = {
+          firstName: "Lucas",
+          email: "LLealL@gmail.com",
+        };
+        return pactum.spec().patch('/users',
+        ).withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        }).withBody(dto).expectStatus(200)
+        .expectBodyContains(dto.firstName)
+        .expectBodyContains(dto.email);
+      });
+    });
   });
 
   
